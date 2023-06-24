e8c4dc87ed959938a78772b428d42276c38ca747
talgreen <talgreen1@yahoo.com>
2023-06-24 17:28:32 +0300

    First push

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import argparse
-def parse_args():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-action', required=True, help='Available Options: save_placeholders')
-    parser.add_argument('-placeholders', required=False, help='list of placeholders details for the mock image.')
-    parser.add_argument('-mock_image_path', type=str,
-                        help='Path for the mock image')
+# import argparse
+import typer
+# def parse_args():
+#     parser = argparse.ArgumentParser(description='Process some integers.')
+#     parser.add_argument('-action', required=True, help='Available Options: save_placeholders')
+#     parser.add_argument('-placeholders', required=False, help='list of placeholders details for the mock image.')
+#     parser.add_argument('-mock_image_path', type=str,
+#                         help='Path for the mock image')
+#
+#     args = parser.parse_args()
+#     print(args.action)
 
-    args = parser.parse_args()
-    print(args.action)
+# parse_args()
+def print_image_placeholder(image_name: str):
+    pass
 
-parse_args()
+if __name__ == "__main__":
+    typer.run(main)

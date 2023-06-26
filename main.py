a47b8b15329a0a97f751072614a2b1fec35aa5c2
talgreen <talgreen1@yahoo.com>
2023-06-26 22:17:31 +0300

    First push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import argparse
 import typer
+from typing_extensions import Annotated
 
 from placeholder import Placeholder
 from tag_utils import get_placeholders, write_placeholders
@@ -7,13 +8,24 @@ from tag_utils import get_placeholders, write_placeholders
 app = typer.Typer()
 
 
+# @app.command()
+# def print_placeholders(image_path: str = Annotated[str, typer.Option("--image-path", "-i")]):
+#     '''
+#     Prints the placeholders which are stored in the image
+#     '''
+#     print(get_placeholders(image_path))
+
+
 @app.command()
-def print_placeholder(image_path: str):
+def print_placeholders(image_path: Annotated[str, typer.Option("--image-path", "-i", help="A full path to the image file.")]):
     print(get_placeholders(image_path))
 
-
 @app.command()
-def set_placeholder(image_path: str, placeholder_list_string):
+def set_placeholders(image_path: Annotated[str, typer.Option("--image-path", "-i", help="A full path to the image file.")],
+                     placeholder_list_string: Annotated[str, typer.Option("--placeholders", "-p", help="A list of placeholders")]):
+    '''
+    Set placeholder(s) to an image
+    '''
     current_placeholder = get_placeholders(image_path)
 
     if current_placeholder:
@@ -32,7 +44,8 @@ def set_placeholder(image_path: str, placeholder_list_string):
     write_placeholders(image_path=image_path, placeholders=placeholder_list)
 
 
-# if __name__ == "__main__":
-#     app()
-print_placeholder(u'./photos/template.jpg')
-set_placeholder(u'./photos/template.jpg', '11,222,3, 4;5,6,7,8')
+if __name__ == "__main__":
+    app()
+
+# print_placeholder(u'./photos/template.jpg')
+# set_placeholder(u'./photos/template.jpg', '11,222,3, 4;5,6,7,8')

f0c54b5e8371f2803a91ffeef379641e0fe3e764
talgreen <talgreen1@yahoo.com>
2023-07-02 22:15:14 +0300

    First push

--- file_utils.py
+++ file_utils.py
@@ -1,7 +1,8 @@
 import os
+import random
 
 
-def get_all_files(folder_path, recursive=True):
+def get_all_files(folder_path, recursive=True, random_order=False):
     all_files = []
     if recursive:
         for root, directories, files in os.walk(folder_path):
@@ -13,6 +14,9 @@ def get_all_files(folder_path, recursive=True):
             file_path = os.path.join(folder_path, file_name)
             if os.path.isfile(file_path):
                 all_files.append(file_path)
+    if random_order:
+        random.shuffle(all_files)
+
     return all_files
 
 
@@ -21,3 +25,6 @@ def create_folder_if_not_exists(path):
 
     if not os.path.exists(folder):
         os.makedirs(folder)
+
+
+# print(*get_all_files('D:\\Dropbox\\My Documents\\github\\python-poster-mockup\\photos\\output', random_order=True),sep='\n')

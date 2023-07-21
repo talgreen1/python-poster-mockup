d7d21df046d01a5ae5b0aaef75b72af72426facc
talgreen <talgreen1@yahoo.com>
2023-07-21 19:42:25 +0300

    First push

--- my_contact_sheets.py
+++ my_contact_sheets.py
@@ -4,17 +4,17 @@ from file_utils import get_all_files
 
 
 def create_contact_sheet(images_path, output_size: int = 1000):
-    images = get_all_files((images_path))
-    contact_sheet = Image.new('RGB', (output_size, output_size))
+    images = get_all_files(images_path)
+    contact_sheet = Image.new('RGB', (output_size, output_size),(255, 255, 255))
 
     x=0
     for image_filepath in images:
             img = Image.open(image_filepath)
-            img = img.resize(500,500)
+            img = img.resize((500,500))
             contact_sheet.paste(img,(0,0))
             x +=100
 
     contact_sheet.save('./my_contact_sheet.jpg')
 
 
-create_contact_sheet('../../photos/items/4')
+create_contact_sheet('./photos/items/4/1')

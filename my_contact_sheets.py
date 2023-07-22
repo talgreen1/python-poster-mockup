69e36ae128df25233bc4ffe193a179fa28d65365
talgreen <talgreen1@yahoo.com>
2023-07-22 17:40:00 +0300

    First push

--- my_contact_sheets.py
+++ my_contact_sheets.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 
 from file_utils import get_all_files
 
@@ -10,11 +10,18 @@ def create_contact_sheet(images_path, output_size: int = 1000):
     x=0
     for image_filepath in images:
             img = Image.open(image_filepath)
-            img = img.resize((500,500))
-            contact_sheet.paste(img,(0,0))
+            # img = img.resize((500,500))
+            img.thumbnail((500,500), Image.Resampling.LANCZOS)
+
+            border_size = 20  # You can adjust this value to control the width of the border
+            border_color = (255, 255, 255)  # (R, G, B) value for white color
+
+            img = ImageOps.expand(img, border=border_size, fill=border_color)
+
+            contact_sheet.paste(img,(x,0))
             x +=100
 
     contact_sheet.save('./my_contact_sheet.jpg')
 
 
-create_contact_sheet('./photos/items/4/1')
+create_contact_sheet('./photos/items/4/2')

7abdf7c551d81d91670a520822a3d06a0b5bce9a
talgreen <talgreen1@yahoo.com>
2023-07-28 12:29:28 +0300

    First push

--- my_contact_sheets.py
+++ my_contact_sheets.py
@@ -10,14 +10,21 @@ def create_contact_sheet(images_path, output_size: int = 1000):
     rows = 0
     cols = 0
 
-    margin = output_size * 0.1
-
+    margin = output_size * 0.01
+    img = Image.open(images[0])
     if len(images) == 2:
         rows = 1
         cols = 2
-    thumb_height = int(output_size / rows - (2 * rows * margin))
+
+    if cols >= rows:
+        thumb_width = int(output_size / cols - (2 * cols * margin))
+        thumb_height = int(thumb_width * img.height / img.width)
+
+
+
+    # thumb_height = int(output_size / rows - (2 * rows * margin))
+    # thumb_width = int(thumb_height * img.width / img.height)
     img = Image.open(images[0])
-    thumb_width = int(thumb_height * img.width / img.height)
     image_index = 0
     x=0
     for r in range(rows):

79adeed1922854fbccb27565f8a5d450ee9ad106
talgreen <talgreen1@yahoo.com>
2023-08-11 17:35:44 +0300

    First push

--- my_contact_sheets_back.py
+++ my_contact_sheets_back.py
@@ -3,30 +3,39 @@ from PIL import Image, ImageOps
 from file_utils import get_all_files
 
 
-def create_contact_sheet(images_path, output_size: int = 1000, margin=0.03):
+def create_contact_sheet(images_path, output_size: int = 1000):
     images = get_all_files(images_path)
     contact_sheet = Image.new('RGB', (output_size, output_size), (255, 255, 255))
 
-    cols_per_row = []
+    rows_cols = []
+    rows = 0
+    cols = 0
 
-
-    margin = output_size * margin
-
+    # margin = output_size * 0.02
+    margin = 20
     img = Image.open(images[0])
     num_of_images = len(images)
     if len(images) == 1:
-        cols_per_row = [1]
+        # rows = 1
+        cols = 1
+        rows_cols = [1]
     elif len(images) == 2:
-        cols_per_row = [2]
+        # rows = 1
+        cols = 2
+        rows_cols = [2]
     elif len(images) == 3:
-        cols_per_row = [3]
+        # rows = 1
+        cols = 3
+        rows_cols = [3]
     elif len(images) == 4:
-        cols_per_row = [2, 2]
+        # rows = 2
+        cols = 2
+        rows_cols = [2, 2]
     elif len(images) == 5:
-        cols_per_row = [3, 2]
+        # rows = 2
+        cols = 3
+        rows_cols = [3, 2]
 
-    rows = len(cols_per_row)
-    cols = max(cols_per_row)
     thumb_width = int((output_size - ((cols + 1) * margin)) / cols)
     thumb_height = int(thumb_width * img.height / img.width)
 
@@ -41,13 +50,29 @@ def create_contact_sheet(images_path, output_size: int = 1000, margin=0.03):
     img = Image.open(images[0])
     image_index = 0
 
+    # x_margin = (output_size - (cols * thumb_width)) / (cols + 1)
+    # y_margin = (output_size - (rows * thumb_height)) / (rows + 1)
+    # y=0
+    # for r in range(rows):
+    #     x = 0
+    #     for c in range(cols):
+    #         img = Image.open(images[image_index])
+    #         img = img.resize((thumb_width, thumb_height), Image.Resampling.LANCZOS)
+    #         image_index += 1
+    #         # print(f'x: {int(x) + int(x_margin)}, y:{int(y)}')
+    #
+    #         contact_sheet.paste(img, (int(x) + int(x_margin), int(y) + int(y_margin)))
+    #         x = (x + thumb_width) + x_margin
+    #     y = (y + thumb_height) + y_margin
+
+    # x_margin = (output_size - (cols * thumb_width)) / (cols + 1)
     y_margin = (output_size - (rows * thumb_height)) / (rows + 1)
 
     y = 0
-    for r in range(rows):
+    for r in range(len(rows_cols)):
         x = 0
-        x_margin = (output_size - (cols_per_row[r] * thumb_width)) / (cols_per_row[r] + 1)
-        for c in range(cols_per_row[r]):
+        x_margin = (output_size - (rows_cols[r] * thumb_width)) / (rows_cols[r] + 1)
+        for c in range(rows_cols[r]):
             img = Image.open(images[image_index])
             img = img.resize((thumb_width, thumb_height), Image.Resampling.LANCZOS)
             image_index += 1

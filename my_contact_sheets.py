dd0ed299947ebc3562351002a3d6561293dfe1ec
talgreen <talgreen1@yahoo.com>
2023-08-12 19:38:13 +0300

    First push

--- my_contact_sheets.py
+++ my_contact_sheets.py
@@ -3,12 +3,11 @@ from PIL import Image, ImageOps, ImageFilter
 from file_utils import get_all_files
 
 
-def create_contact_sheet(images_path, output_size: int = 1000, margin=0.03):
+def create_contact_sheet(images_path, output_size: int = 1000, margin=0.03, with_shadows=True):
     images = get_all_files(images_path)
     contact_sheet = Image.new('RGBA', (output_size, output_size), (255, 255, 255))
 
     cols_per_row = []
-
 
     margin = output_size * margin
 
@@ -41,6 +40,35 @@ def create_contact_sheet(images_path, output_size: int = 1000, margin=0.03):
     img = Image.open(images[0])
     image_index = 0
 
+    if with_shadows:
+        contact_sheet_shadows = Image.new('RGBA', (output_size, output_size), (255, 255, 255))
+        # shadow_size = (img.width, img.height)  # Adjust shadow size as needed
+        shadow_color = (0, 0, 0, 100)  # RGBA color for the shadow (adjust alpha for intensity)
+        shadow = Image.new('RGBA', (thumb_width, thumb_height), shadow_color)
+        shadow_offset = 15
+
+        y_margin = (output_size - (rows * thumb_height)) / (rows + 1)
+
+        y = 0
+        for r in range(rows):
+            x = 0
+            x_margin = (output_size - (cols_per_row[r] * thumb_width)) / (cols_per_row[r] + 1)
+            for c in range(cols_per_row[r]):
+                # img = Image.open(images[image_index])
+                # img = img.resize((thumb_width, thumb_height), Image.Resampling.LANCZOS)
+                # image_index += 1
+
+                contact_sheet_shadows.paste(shadow, (int(x) + int(x_margin)+shadow_offset, int(y) + int(y_margin)+shadow_offset))
+                x = (x + thumb_width) + x_margin
+            y = (y + thumb_height) + y_margin
+
+        blurred_contact_sheet_shadows = contact_sheet_shadows.filter(ImageFilter.GaussianBlur(10))
+        contact_sheet = Image.new('RGBA', (output_size, output_size), (255, 255, 255))
+
+        contact_sheet = Image.alpha_composite(contact_sheet, blurred_contact_sheet_shadows)
+        # contact_sheet.show()
+
+
     y_margin = (output_size - (rows * thumb_height)) / (rows + 1)
 
     y = 0
@@ -51,31 +79,13 @@ def create_contact_sheet(images_path, output_size: int = 1000, margin=0.03):
             img = Image.open(images[image_index])
             img = img.resize((thumb_width, thumb_height), Image.Resampling.LANCZOS)
             image_index += 1
-            # print(f'x: {int(x) + int(x_margin)}, y:{int(y)}')
-
-            ############################################################# Add shadow
-            shadow_color = (0, 0, 0, 100)  # RGBA color for the shadow
-            shadow_offset = (10, 10)  # Offset of the shadow (x, y)
-
-            shadow_image = Image.new("RGBA", img.size, (0, 0, 0, 100))  # RGBA color and transparency
-            # contact_sheet.paste(shadow_image, (shadow_offset[0], shadow_offset[1]), shadow_image)
-            shadow_image = shadow_image.filter(ImageFilter.GaussianBlur(5))
-            shadow_image = Image.alpha_composite(shadow_image, shadow_image)
-
-
-            contact_sheet.paste(shadow_image,
-                                (int(x) + int(x_margin) + shadow_offset[0], int(y) + int(y_margin) + shadow_offset[1]),
-                                shadow_image)
-            blurred_shadow = contact_sheet.filter(ImageFilter.GaussianBlur(5))
-            # contact_sheet = Image.alpha_composite(contact_sheet, blurred_shadow)
-            ###################################################################
-
 
             contact_sheet.paste(img, (int(x) + int(x_margin), int(y) + int(y_margin)))
             x = (x + thumb_width) + x_margin
         y = (y + thumb_height) + y_margin
 
     contact_sheet.save('./my_contact_sheet.png')
+    # contact_sheet.show()
 
 
-create_contact_sheet('photos/items/4')
+create_contact_sheet('photos/items/4', with_shadows=False)
